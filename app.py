6d5a0cf07f41d9529311b785eb581096fc8aefb8
Ilona S <ilonas@MacBook-Air-Ilona.local>
2024-10-25 21:55:06 +0200

    Initial commit: add and subtract functionality for calculator

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    operation = sys.argv[1]
+    num1 = int(sys.argv[2])
+    num2 = int(sys.argv[3])
+
+    if operation == "add":
+        print(add(num1, num2))
+    elif operation == "subtract":
+        print(subtract(num1, num2))
+    else:
+        print("Unsupported operation")
